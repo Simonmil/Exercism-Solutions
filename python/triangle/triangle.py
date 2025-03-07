0cf04e529b4b4d100041ed2dedb3d883be4175fe
Simon Millerjord <simonmillerjord@gmail.com>
2025-03-07 14:14:19 -0800

    Solved the exercise

--- python/triangle/triangle.py
+++ python/triangle/triangle.py
@@ -1,10 +1,12 @@
-def equilateral(sides):
-    pass
+def is_triangle(sides):
+    a, b, c = sides
+    return a + b >= c and b + c >= a and c + a >= b and all(sides)
 
+def equilateral(sides):
+    return len(set(sides)) == 1 and is_triangle(sides)
 
 def isosceles(sides):
-    pass
-
+    return len(set(sides)) < 3 and is_triangle(sides)
 
 def scalene(sides):
-    pass
+    return len(set(sides)) == 3 and is_triangle(sides)
