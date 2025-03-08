a59bee228b1f8546cb7be583172e13c7b796df08
Simon Millerjord <simonmillerjord@gmail.com>
2025-03-07 17:39:58 -0800

    Completed Armstrong Numbers exercise

--- python/armstrong-numbers/armstrong_numbers.py
+++ python/armstrong-numbers/armstrong_numbers.py
@@ -1,2 +1,6 @@
 def is_armstrong_number(number):
-    pass
+    sum = 0
+    number_as_string = str(number)
+    for digit in number_as_string:
+        sum += int(digit) ** len(number_as_string)
+    return sum == number
