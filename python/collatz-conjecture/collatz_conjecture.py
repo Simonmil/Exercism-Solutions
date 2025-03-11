c17b8f2d05905f1ce91dd98042ed4685830f95f6
Simon Millerjord <simonmillerjord@gmail.com>
2025-03-10 19:42:38 -0700

    Completed the exercise Collatz Conjecture

--- python/collatz-conjecture/collatz_conjecture.py
+++ python/collatz-conjecture/collatz_conjecture.py
@@ -1,2 +1,13 @@
 def steps(number):
-    pass
+    if (number < 1):
+        raise ValueError("Only positive integers are allowed")
+    steps = 0
+    while number > 1:
+        match number % 2:
+            case 0:
+                number = number/2
+            case 1:
+                number = number*3 + 1 
+        steps += 1
+
+    return steps
