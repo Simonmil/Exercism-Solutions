78e9f6b6a1595835d26d51a6d79f235d02434839
Simon Millerjord <simonmillerjord@gmail.com>
2025-03-10 20:09:57 -0700

    Completed the exercise, but not tested. Might be optimizable

--- python/bob/bob.py
+++ python/bob/bob.py
@@ -1,2 +1,11 @@
 def response(hey_bob):
-    pass
+    if "?" in hey_bob:
+        return "Sure."
+    elif hey_bob == hey_bob.upper():
+        return "Whoa, chill out!"
+    elif hey_bob == hey_bob.upper() and "?" in hey_bob:
+        return "Calm down, I know what I'm doing!"
+    elif len(hey_bob) == 0:
+        return "Fine. Be that way!"
+    else:
+        return "Whatever."
