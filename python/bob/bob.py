144ea6dd227d3261a2557efdbd856dcc2ef08999
Simon Millerjord <simonmillerjord@gmail.com>
2025-03-14 17:42:24 -0700

    Started exercise, but must think more on edge cases

--- python/bob/bob.py
+++ python/bob/bob.py
@@ -1,11 +1,11 @@
 def response(hey_bob):
-    if "?" in hey_bob:
-        return "Sure."
+    if len(hey_bob) == 0:
+        return "Fine. Be that way!"
+    elif  hey_bob == hey_bob.upper() and "?" in hey_bob:
+        return "Calm down, I know what I'm doing!"
     elif hey_bob == hey_bob.upper():
         return "Whoa, chill out!"
-    elif hey_bob == hey_bob.upper() and "?" in hey_bob:
-        return "Calm down, I know what I'm doing!"
-    elif len(hey_bob) == 0:
-        return "Fine. Be that way!"
+    elif hey_bob[-1] == "?":
+        return "Sure."
     else:
         return "Whatever."
