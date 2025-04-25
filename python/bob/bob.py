cf749107a7d6efc495375bce8da782a8380c324a
Simon Millerjord <simonmillerjord@gmail.com>
2025-04-25 16:14:31 -0700

    Completed the exercise

--- python/bob/bob.py
+++ python/bob/bob.py
@@ -1,11 +1,13 @@
 def response(hey_bob):
     hey_bob = hey_bob.strip()
+    #return "foobar"
     if len(hey_bob) == 0:
         return "Fine. Be that way!"
-    elif  hey_bob == hey_bob.upper() and "?" in hey_bob:
-        return "Calm down, I know what I'm doing!"
-    elif hey_bob == hey_bob.upper():
-        return "Whoa, chill out!"
+    elif hey_bob == hey_bob.upper() and hey_bob.lower() != hey_bob.upper():
+        if hey_bob[-1] == "?":
+            return "Calm down, I know what I'm doing!"
+        else:
+            return "Whoa, chill out!"
     elif hey_bob[-1] == "?":
         return "Sure."
     else:
