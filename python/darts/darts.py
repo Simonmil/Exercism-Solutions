fc3998d9cd104b481e8ee018f0543566666356fe
Simon Millerjord <simonmillerjord@gmail.com>
2025-04-25 16:48:11 -0700

    Completed exercise

--- python/darts/darts.py
+++ python/darts/darts.py
@@ -1,2 +1,16 @@
+import math
+
 def score(x, y):
-    pass
+    """
+    I want to use the Pytagorian theorem to calculate the distance. 
+    """
+    distance = math.sqrt(x**2 + y**2)
+    if 10 < distance:
+        return 0
+    if 5 < distance <= 10:
+        return 1
+    if 1 < distance <= 5:
+        return 5
+    if distance <= 1:
+        return 10
+
