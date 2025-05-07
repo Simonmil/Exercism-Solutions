30a151f69be1477913ebe375e81377305fc6bea1
Simon Millerjord <simonmillerjord@gmail.com>
2025-05-07 15:49:16 -0700

    Added a better solution

--- python/reverse-string/reverse_string.py
+++ python/reverse-string/reverse_string.py
@@ -3,3 +3,9 @@ def reverse(text):
     for index in range(1,len(text)+1):
         reversed_text += text[-index]
     return reversed_text
+
+    """
+    Better solution:
+    
+    return text[::-1]
+    """
