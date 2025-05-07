841e506d9fe4162ff0efbcc0bd574f2bc9e096df
Simon Millerjord <simonmillerjord@gmail.com>
2025-05-07 15:29:33 -0700

    Completed the exercise

--- python/perfect-numbers/perfect_numbers.py
+++ python/perfect-numbers/perfect_numbers.py
@@ -4,4 +4,18 @@ def classify(number):
     :param number: int a positive integer
     :return: str the classification of the input integer
     """
-    pass
+    if number < 1: 
+        raise ValueError("Classification is only possible for positive integers.")
+    
+    denum = 1
+    factor_sum = 0
+    while denum < number:
+        if number % denum == 0:
+            factor_sum += denum
+        denum += 1
+
+    if factor_sum == number:
+        return 'perfect'
+    elif factor_sum < number:
+        return 'deficient'
+    return 'abundant'
