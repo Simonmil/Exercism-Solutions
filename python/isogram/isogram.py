871f54ef70d2860eeb5b67cef2ab7814d0dedaaf
Simon Millerjord <simonmillerjord@gmail.com>
2025-05-16 14:27:14 -0700

    Completed exercise

--- python/isogram/isogram.py
+++ python/isogram/isogram.py
@@ -1,2 +1,3 @@
 def is_isogram(string):
-    pass
+    string = string.replace(" ","").lower()
+    return all(string.count(character) == 1 and character.isalpha() or not character.isalpha() for character in string)
