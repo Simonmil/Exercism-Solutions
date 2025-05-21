1f664df4c5db7fc919bf920128684ab989db399a
Simon Millerjord <simonmillerjord@gmail.com>
2025-05-21 10:45:51 -0700

    Completed exercise

--- python/rna-transcription/rna_transcription.py
+++ python/rna-transcription/rna_transcription.py
@@ -1,2 +1,17 @@
 def to_rna(dna_strand):
-    pass
+    rna_strand = ""
+    for nucleotide in dna_strand:
+        match nucleotide:
+            case "G":
+                rna_strand += "C"
+            case "C":
+                rna_strand += "G"            
+            case "T":
+                rna_strand += "A"            
+            case "A":
+                rna_strand += "U"
+            #case _:
+                #return raise ValueError("Unknown nucleotide in DNA sequence") 
+    
+    return rna_strand
+            
