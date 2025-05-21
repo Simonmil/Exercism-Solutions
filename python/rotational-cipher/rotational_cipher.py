1b09fc6299d5c403e59221cea81269173e107723
Simon Millerjord <simonmillerjord@gmail.com>
2025-05-20 20:50:53 -0700

    Completed exercise. Got help from the AI, need to remeber this.

--- python/rotational-cipher/rotational_cipher.py
+++ python/rotational-cipher/rotational_cipher.py
@@ -1,2 +1,20 @@
+import string
+
 def rotate(text, key):
-    pass
+    lower = string.ascii_lowercase
+    upper = string.ascii_uppercase
+    code = []
+    if key == 26: key = 0
+
+    for letter in text:
+        match letter:
+            case l if l in lower:
+                idx = lower.index(l)
+                code.append(lower[(idx + key) % 26])
+            case l if l in upper:
+                idx = upper.index(l)
+                code.append(upper[(idx + key) % 26])
+            case _:
+                code.append(letter)
+
+    return ''.join(code)
