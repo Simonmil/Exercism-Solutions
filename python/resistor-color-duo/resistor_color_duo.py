52891577ccd13e4161627414155a3260c68287f4
Simon Millerjord <simonmillerjord@gmail.com>
2025-05-21 14:59:56 -0700

    Completed exercise

--- python/resistor-color-duo/resistor_color_duo.py
+++ python/resistor-color-duo/resistor_color_duo.py
@@ -1,2 +1,7 @@
 def value(colors):
-    pass
+    color_codes = ['black','brown','red','orange','yellow','green','blue','violet','grey','white']
+    colors = colors[:2]
+    code = ''
+    for color in colors:
+        code += str(color_codes.index(color))
+    return int(code)
