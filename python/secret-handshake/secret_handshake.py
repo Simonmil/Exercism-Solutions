8441ca116abba3221a96bb65f7d21209e731bc8f
Simon Millerjord <simonmillerjord@gmail.com>
2025-05-26 13:59:17 -0700

    Completed exercise

--- python/secret-handshake/secret_handshake.py
+++ python/secret-handshake/secret_handshake.py
@@ -1,2 +1,11 @@
 def commands(binary_str):
-    pass
+    actions = ['wink','double blink','close your eyes','jump','reverse actions']
+    actions_to_do = []
+    for index,binary in enumerate(binary_str[::-1]):
+        if binary == '1': 
+            print(index,binary)
+            if index == 4:
+                actions_to_do.reverse()
+            else:
+                actions_to_do.append(actions[index])
+    return actions_to_do
