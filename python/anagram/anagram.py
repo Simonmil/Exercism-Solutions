0d2d3d0eaffa15df94eecaaae70753599ae204f7
Simon Millerjord <simonmillerjord@gmail.com>
2025-05-26 17:14:34 -0700

    Almost there!

--- python/anagram/anagram.py
+++ python/anagram/anagram.py
@@ -2,8 +2,8 @@ def find_anagrams(word, candidates):
     word = word.lower()
     anagrams = []
     for candidate in candidates:
-        candidate = candidate.lower()
-        if all(letter in word for letter in candidate) and word != candidate:
+        #candidate = candidate.lower()
+        if all(letter.lower() in word for letter in candidate) and word != candidate.lower() and len(word) == len(candidate):
             anagrams.append(candidate)
-
+    print(anagrams)
     return anagrams
